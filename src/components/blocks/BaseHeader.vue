5e3886d9fbfdaace0f7da5814be95d5fb31469b3
Nothung429 <frank.gatto96.fg@gmail.com>
2022-06-07 15:23:44 +0200

    add files

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <div class="header__logo">
+            <img src="../../assets/img/spotify-logo.png" alt="">
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    header {
+        background-color: #2e3a46;
+        height: 80px;
+        padding: 0 20px;
+        display: flex;
+        align-items: center;
+        .header__logo {
+            height: 70px;
+            width: 80px;
+            img {
+                max-width: 100%;
+            }
+        }
+    }
+</style>
